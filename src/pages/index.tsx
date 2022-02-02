493da174c475d45da6c5f8421b00c2bf8426da68
Marcello Lopes <marcell0lopes@outlook.com>
2022-02-02 19:00:41 -0300

    subscribe button layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
               for $9.90<small>/month</small>
             </span>
           </p>
+          <SubscribeButton />
         </section>
 
         <Image
