c357b341f38a50c5a02a59894c7a7cf5b7fb8f34
Marcello Lopes <marcell0lopes@outlook.com>
2022-03-30 23:49:46 -0300

    updated post from prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -9,38 +24,51 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2022</time>
-            <strong>Titulo do post</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates ea ipsa placeat officia alias repellat quos et, nostrum
-              laborum fugit nisi! Voluptas, totam. Eum ea corporis dignissimos,
-              explicabo dolorem at!
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2022</time>
-            <strong>Titulo do post</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates ea ipsa placeat officia alias repellat quos et, nostrum
-              laborum fugit nisi! Voluptas, totam. Eum ea corporis dignissimos,
-              explicabo dolorem at!
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2022</time>
-            <strong>Titulo do post</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates ea ipsa placeat officia alias repellat quos et, nostrum
-              laborum fugit nisi! Voluptas, totam. Eum ea corporis dignissimos,
-              explicabo dolorem at!
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href={post.slug} key={post.title}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query<any>(
+    [Prismic.Predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(response);
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-br",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
